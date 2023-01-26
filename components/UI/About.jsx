e26606b7c3f5d94e899e6bcd1d992eb3e1c3aeeb
Christodule <zcedric121@gmail.com>
2023-01-26 12:51:37 -0600

    first commit

---
+++ components/UI/About.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Link from "next/link";
+import SectionSubtitle from "./SectionSubtitle";
+import Image from "next/image";
+
+import img01 from "../../public/images/img-01.jpg";
+import img02 from "../../public/images/img-02.jpg";
+import img03 from "../../public/images/img-03.jpg";
+import img04 from "../../public/images/hero.jpg";
+
+import classes from "../../styles/about.module.css";
+
+const About = () => {
+  return (
+    <section id="about">
+      <Container>
+        <Row>
+          <Col lg="6" className={`${classes.about__content}`}>
+            <SectionSubtitle subtitle="A propos" />
+            <h3 className="mt-4">Je suis </h3>
+            <h3 className="mb-4">Trader professionnel analyste financier et investisseur</h3>
+            <p>
+           J'investi dans plusieurs marchés financiers notament ceux du forex (EUR/USD), les actions (Nasdaq100; CAC40; DAX30...) les matieres premieres (Or; petrole , brent...) mais aussi en tant qu'entrepreneur dans les domaines de l'elevage, et de lagriculture et de la musique!
+            </p>
+
+            <div className=" d-flex align-items-center gap-5">
+              <div>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                   Accompagnements
+                </h6>
+
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Recherches
+                </h6>
+              </div>
+
+              <div>
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Créations
+                </h6>
+
+                <h6 className=" d-flex align-items-center gap-2 mt-3 fw-normal">
+                  <span className={`${classes.about__icon}`}>
+                    <i className="ri-checkbox-circle-line"></i>
+                  </span>
+                  Qualité
+                </h6>
+              </div>
+            </div>
+
+            <div className="mt-5">
+              <button className="primary__btn">
+                <Link href="#portfolio">Mon Portfolio</Link>
+              </button>
+
+           
+            </div>
+          </Col>
+
+          <Col lg="6">
+            <div
+              className={`${classes.about__img__gallery} d-flex gap-4 justify-content-end`}
+            >
+              <div className=" d-flex flex-column mb-3">
+                <div
+                  className={`${classes.about__img} ${classes.about__img__box}`}
+                >
+                  <Image src={img01} alt="about-img" />
+                </div>
+
+                <div
+                  className={`${classes.about__img} ${classes.about__img__box}`}
+                >
+                  <Image src={img02} alt="about-img" />
+                </div>
+              </div>
+
+              <div className=" d-flex flex-column mb-3">
+                <div
+                  className={`${classes.about__img} ${classes.about__img__box}`}
+                >
+                  <Image src={img03} alt="about-img" />
+                </div>
+
+                <div
+                  className={`${classes.about__img} ${classes.about__img__box}`}
+                >
+                  <Image src={img04} alt="about-img" />
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
---
+++ components/data/portfolio.js
@@ -0,0 +1,31 @@
+const portfolio = [
+  {
+    id: "01",
+    title: "Agir foundation corporation",
+    img: "/images/portfolio-01.jpg",
+    category: "Mobile App",
+    keyword: ["Mobile", "App", "UI-UX"],
+    liveUrl: "#",
+  },
+
+  {
+    id: "02",
+    title: "Realisations de couveuses",
+    img: "/images/portfolio-02.jpg",
+    category: "Mobile App",
+    keyword: ["Mobile", "App", "UI-UX"],
+    liveUrl: "#",
+  },
+
+  {
+    id: "03",
+    title: "Realisations de couveuses",
+    img: "/images/portfolio-03.jpg",
+    category: "Mobile App",
+    keyword: ["Mobile", "App", "UI-UX"],
+    liveUrl: "#",
+  },
+
+];
+
+export default portfolio;
